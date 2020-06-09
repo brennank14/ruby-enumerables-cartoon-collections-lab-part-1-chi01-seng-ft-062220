9e08e913a0d99c33858dfbe2d0b0d0722ff516e1
Kathleen Brennan <brennank14@gmail.com>
2020-06-09 15:35:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,14 @@
 oppressed_workers = ["dopey", "sneezy", "happy", "angry", "doc", "lemonjello", "sleepy" ]
 
 def greet_characters(array)
-  array.each do |element|
-    puts "Hello #{element}!"
-  end
+  array.each {|element| puts "Hello #{element}!"}
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index {|element, index| puts "#{index + 1} #{element}"
-  }
+  array.each_with_index {|element, index| puts "#{index + 1} #{element}"}
 end
 
 list_dwarves(oppressed_workers)
