f182a803deec06fc09263d5ee6d289b5b1fa6a1a
Kathleen Brennan <brennank14@gmail.com>
2020-06-09 15:31:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,7 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index do |element|
-    puts "#{index}. #{element}"
+  array.each_with_index do {|element, index| puts "#{index}. #{element}"}
 end
 
 list_dwarves(oppressed_workers)
